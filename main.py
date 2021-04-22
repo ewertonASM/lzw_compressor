5a3196d697936a76c8395bb486e7bb0c7dcc2e60
ewertonASM <ewerton-andre@outlook.com>
2021-04-22 11:09:38 -0300

    feature: dynamic dictionary size

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import fire
 
 
 def process(**kwargs):
-    
+
     try:
         option = kwargs.get('operation')
 
@@ -14,14 +14,16 @@ def process(**kwargs):
 
         file_dir = kwargs.get('input_file')
         bits_number = kwargs.get('bits_number')
-        dictionary_size = kwargs.get('dictionary_size')
+        # dictionary_size = kwargs.get('dictionary_size')
 
-        compress = lzwcompress.LzwCompress(file_dir, bits_number, dictionary_size)
+        compress = lzwcompress.LzwCompress(file_dir, bits_number)
+        print(f'K={bits_number}, Dictionary_size={pow(2,bits_number)}')
+        print('COMPRESSING')
+
         compress.start_compress()
 
     elif option == 'decompress':
         pass
-    
 
 
 if __name__ == '__main__':
--- util/lzwcompress.py
+++ util/lzwcompress.py
@@ -1,22 +1,22 @@
 from pathlib import Path
 from struct import pack
+from tqdm import tqdm
 import fire
 import re
 
 
 class LzwCompress():
 
-    def __init__(self, file_dir=str, bits_number=int, dictionary_size=int):
+    def __init__(self, file_dir=str, bits_number=int):
 
         self._file_dir = file_dir
-        self._table_edge = pow(2,bits_number)
-        self._dictionary_size = dictionary_size                  
-        self._dictionary = {chr(i): i for i in range(dictionary_size)}
+        self._table_edge = pow(2, bits_number)
+        self.aux_dictionary_size = 256
+        self._dictionary = {chr(i): i for i in range(self.aux_dictionary_size)}
 
-    
     def open_file(self):
 
-        with open(self._file_dir) as f:
+        with open(self._file_dir, encoding='latin-1') as f:
             data = f.read()
 
         return data
@@ -30,20 +30,20 @@ class LzwCompress():
         with open(f'./output/{output_dir}', 'wb') as output:
 
             for data in compressed_data:
-                output.write(pack('>H',int(data)))
+                output.write(pack('>H', int(data)))
 
     def start_compress(self):
-        
+
         data = self.open_file()
-        
-        string = ""            
+
+        string = ""
         compressed_data = []
 
-        for character in data:  
+        for character in tqdm(data):
 
             symbol = string + character
 
-            if symbol in self._dictionary: 
+            if symbol in self._dictionary:
                 string = symbol
 
             else:
@@ -52,15 +52,13 @@ class LzwCompress():
 
                 if(len(self._dictionary) <= self._table_edge):
 
-                    self._dictionary[symbol] = self._dictionary_size
-                    self._dictionary_size += 1
+                    self._dictionary[symbol] = self.aux_dictionary_size
+                    self.aux_dictionary_size += 1
 
                 string = character
 
         if string in self._dictionary:
 
             compressed_data.append(self._dictionary[string])
-        
+
         self.write_compress_file(compressed_data)
-
-
